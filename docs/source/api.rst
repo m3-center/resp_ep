c0ce72ffd5a9bddc21c653ec3f4dc5ecd35465c6
karl <karl.kirschner@h-brs.de>
2026-04-30 11:14:06 +0200

    Updated sphinx documentation making to include all scripts.

--- docs/source/api.rst
+++ docs/source/api.rst
@@ -4,5 +4,14 @@ RESP API
 
 .. automodapi:: resp_ep
 
+.. automodapi:: resp_ep.driver
+
 .. automodapi:: resp_ep.espfit
 
+.. automodapi:: resp_ep.extras
+
+.. automodapi:: resp_ep.stage2_helper
+
+.. automodapi:: resp_ep.vdw_surface
+
+.. automodapi:: tests.test_resp_ep
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -119,7 +119,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'respdoc'
+htmlhelp_basename = 'resp_epdoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -146,8 +146,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'resp.tex', 'resp Documentation',
-     'Asem Alenaizan', 'manual'),
+    (master_doc, 'resp_ep.tex', 'resp_ep Documentation',
+     'Karl N. Kirschner', 'manual'),
 ]
 
 
@@ -156,7 +156,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'resp', 'resp Documentation',
+    (master_doc, 'resp_ep', 'resp_ep Documentation',
      [author], 1)
 ]
 
@@ -167,8 +167,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'resp', 'resp Documentation',
-     author, 'resp', 'One line description of project.',
+    (master_doc, 'resp_ep', 'resp_ep Documentation',
+     author, 'resp_ep', 'One line description of project.',
      'Miscellaneous'),
 ]
 
